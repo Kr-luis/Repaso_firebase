5bf4c41fb0076cab2989efc98676597abb0bb145
Kr-luis <kluis816@gmail.com>
2024-11-14 08:55:58 -0500

    Revert "LGuaygua - primera version autenticacion"
    This reverts commit b0773375bf8d4bd3f68316dde9605e2845737fc0.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,30 +1,22 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
-import { redirectUnauthorizedTo, redirectLoggedInTo, canActivate } from '@angular/fire/auth-guard';
-
-const redirectUnauthorizedToLogin = () => redirectUnauthorizedTo(['']);
-const redirectLoggedInToHome = () => redirectLoggedInTo(['home']);
 
 const routes: Routes = [
-	{
-		path: '',
-		loadChildren: () => import('./login/login.module').then((m) => m.LoginPageModule),
-		...canActivate(redirectLoggedInToHome)
-	},
-	{
-		path: 'home',
-		loadChildren: () => import('./home/home.module').then((m) => m.HomePageModule),
-		...canActivate(redirectUnauthorizedToLogin)
-	},
-	{
-		path: '**',
-		redirectTo: '',
-		pathMatch: 'full'
-	}
+  {
+    path: 'home',
+    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+  },
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
 ];
 
 @NgModule({
-	imports: [RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })],
-	exports: [RouterModule]
+  imports: [
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+  ],
+  exports: [RouterModule]
 })
-export class AppRoutingModule {}
+export class AppRoutingModule { }
